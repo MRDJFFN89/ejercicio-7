772b4e79120778e1a75c99483a016a627b933c45
MRDJFFN89 <57576452+MRDJFFN89@users.noreply.github.com>
2019-11-14 13:21:42 -0600

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> javascript</title>
+	<style type="text/css" href=""></style>
+	 <link rel="stylesheet" type="text/css" href="Estilo.css">
+     <link rel="stylesheet" type="text/css" href="definitivo.css">  
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<h1 class="estilo"> <img src="https://www.posicionamiento-web-salamanca.com/wp-content/uploads/2016/01/programacion-web-destacada.png" class="imagen2" /> Programacion WEB </h1>
+    
+   
+  
+   <ul>
+   <li ><a href="index.html" class="active">inicio </li></a>
+   <li ><a href="css.html">css</li></a>
+   <li ><a href="Javascript.html">Javascript</li></a>
+   <li ><a href="HTML.html">Html</li></a>
+   <li><a href="Angular.html">Angular</li></a>
+
+
+   </ul>
+
+   <section class="section-estile">
+   <h3 class="h3-e">Javascript</h3>
+   <div align="center"><br/>
+   <img src="C:\Users\austi\Pictures\javascriptlogo-2.jpg" alt="error"  class="imagen" />
+   </div><br/>
+
+   	<p class="p"> JavaScript (abreviado comúnmente JS) es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos,3​ basado en prototipos, imperativo, débilmente tipado y dinámico.
+
+Se utiliza principalmente en su forma del lado del cliente (client-side), implementado como parte de un navegador web permitiendo mejoras en la interfaz de usuario y páginas web dinámicas4​ aunque existe una forma de JavaScript del lado del servidor (Server-side JavaScript o SSJS). Su uso en aplicaciones externas a la web, por ejemplo en documentos PDF, aplicaciones de escritorio (mayoritariamente widgets) es también significativo.
+
+Desde el 2012, todos los navegadores modernos soportan completamente ECMAScript 5.1, una versión de javascript. Los navegadores más antiguos soportan por lo menos ECMAScript 3. La sexta edición se liberó en julio del 2015.5​
+
+JavaScript se diseñó con una sintaxis similar a C, aunque adopta nombres y convenciones del lenguaje de programación Java. Sin embargo, Java y JavaScript tienen semánticas y propósitos diferentes.
+
+Todos los navegadores modernos interpretan el código JavaScript integrado en las páginas web. Para interactuar con una página web se provee al lenguaje JavaScript de una implementación del Document Object Model (DOM).
+
+Tradicionalmente se venía utilizando en páginas web HTML para realizar operaciones y únicamente en el marco de la aplicación cliente, sin acceso a funciones del servidor. Actualmente es ampliamente utilizado para enviar y recibir información del servidor junto con ayuda de otras tecnologías como AJAX. JavaScript se interpreta en el agente de usuario al mismo tiempo que las sentencias van descargándose junto con el código HTML.
+
+Desde el lanzamiento en junio de 1997 del estándar ECMAScript 1, han existido las versiones 2, 3 y 5, que es la más usada actualmente (la 4 se abandonó6​). En junio de 2015 se cerró y publicó la versión ECMAScript 6.7​</p>
+
+
+
+<br/><br/><br/><footer class="fooestilo" > 
+	Derechos reservados 2019 | javascript
+  </footer> 
+
+
+</section>
+
+</body>
+</html>
