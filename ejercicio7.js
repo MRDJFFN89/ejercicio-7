95aefbbf0b5d7f356e24d9c45d5aed7e984dcd6b
MRDJFFN89 <57576452+MRDJFFN89@users.noreply.github.com>
2019-11-14 13:08:26 -0600

    Update ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -12,6 +12,12 @@ for(var i=1; i<=10; i++){
 	document.write(i + "x " +  numero  + " = "  + (i * numero ) + "<br/>");
 }
 
+for(var c=1; c<=10; c++){
+	document.write("<h1> tabla del " + c + "</h1>");
+for(i=1; i<=10; i++){
+	document.write(i + "x " +  c  + " = "  + (i * c ) + "<br/>");
+}
+ }
 
 
 
